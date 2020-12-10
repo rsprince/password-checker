5093030538c955e451c81488c921c131fac586c3
Richard Sprince <sprince.richard@gmail.com>
2020-12-10 12:44:56 -0500

    Simplifying loop.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -27,41 +27,43 @@ export class Tab1Page {
   }
 	
 	checkPassword(password) {
+		let teststring;
 		this.tries = 0;
 		this.t0 = performance.now();
 		// instantiate match in root to enable return on match
 		let match;
 		// test first char
-    for(let a=0; a < this.characters.length; a++) {
+		for(let a=0; a < this.characters.length; a++) {
 			//test second char
 			for(let b=0; b < this.characters.length; b++) {
 				// test third char
 				for(let c=0; c < this.characters.length; c++) {
 					// test fourth char
 					for(let d=0; d < this.characters.length; d++) {
-						console.log(this.characters[a], this.characters[b], this.characters[c], this.characters[d]);
+						teststring = this.characters[a] + this.characters[b] + this.characters[c] + this.characters[d];
 						this.tries++;
 						console.log("Tries: ", this.tries);
-						let match = this.matchPassword(password, a, b, c, d);
+						console.log("teststring: ", teststring);
+						let match = this.matchPassword(password, teststring);
 						if(match) return;
 					}
 				}
 			}
 			if(match) return;
-    }
+		}
 	}
 
-	matchPassword(password, a, b, c, d) {
-		let testString = this.characters[a] + this.characters[b] + this.characters[c] + this.characters[d];
-		if(testString == password) {
-			console.log(testString + " is a match.");
+	matchPassword(password, teststring) {
+		//this.testString = this.characters[a] + this.characters[b] + this.characters[c] + this.characters[d];
+		if(teststring == password) {
+			console.log(teststring + " is a match.");
 			let t1 = performance.now();
 			console.log("The match took " + (t1 - this.t0) + " milliseconds.");
 			return true;
 		}
 	}
 
-	enterPassword(form) {
+	enterPassword() {
 		console.log(this.passwordForm.value.password);
 		this.checkPassword(this.passwordForm.value.password);
 	}
